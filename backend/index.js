f5d1f88ad23d247b674f5cdfac332b27c4c235ca
Anshul Anand <mr.anshuking@gmail.com>
2026-02-10 12:28:48 +0530

    Fix port for Render deployment

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // JSON body read karne ke liye middleware
 app.use(express.json());
